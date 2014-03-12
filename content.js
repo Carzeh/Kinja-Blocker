0e76af55f2c9db85880e737d59c08953fcd46e04
Adam Burvill <runtothehills@gmail.com>
2014-03-12 14:53:54 -0400

    Reverting back

--- content.js
+++ content.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function(){
 	// Check to see if there is blocked list on page load and filter if there is...
 	chrome.runtime.sendMessage({type: "list on page load"},function(response){
@@ -24,14 +23,21 @@ function Filtering(person){
 		}
 	});
 
-	// Filters input for "Tiger" - the new Kinja platform
-	$("a:containsNC('" + person + "')").closest('.post-wrapper.js_post-wrapper').remove();
+	// Check for "Tiger" - the new Kinja platform
+
+	// if ($(body.f_tiger_on).parent('html')) {
+		
+	// 	console.log('test');
+	// }
+
+	// else {
 
 	// Filters input for "old" pre-Tiger pages
+	$("a:containsNC('" + person + "')").closest('.post-wrapper.js_post-wrapper').remove();
 	$( "a:containsNC('" + person + "')" ).closest('.post-wrapper.js_post-wrapper.wide.postlist-dense').remove();
 	$( ".text-upper:containsNC('" + person + "')" ).closest('.post-wrapper.js_post-wrapper.wide.postlist-dense').remove();
 	$( "a:containsNC('" + person + "')" ).closest('.post-wrapper.js_post-wrapper.postlist-dense').remove(); 
-	
+	// }
 };
 
 
@@ -44,48 +50,4 @@ chrome.runtime.onMessage.addListener(function(msg,sender){
 
 	}
 			
-=======
-$(document).ready(function(){
-	// Check to see if there is blocked list on page load and filter if there is...
-	chrome.runtime.sendMessage({type: "list on page load"},function(response){
-		PageLoadFiltering(response.blocked);
-	});
-});
-
-// Filter gawker page using blocked list
-
-function PageLoadFiltering(personList){
-	for (var i=0;i<personList.length;i++){
-			Filtering(personList[i]);	
-	};
-};
-
-// Alter Gawker homepage HTML via jQuery to remove author passed as argument to Filtering
-
-function Filtering(person){
-
-	// Removes input case sensitivity
-	$.extend($.expr[":"], {"containsNC": function(elem, i, match, array) {
-		return (elem.textContent || elem.innerText || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
-		}
-	});
-
-	// Filters input
-	$( "a:containsNC('" + person + "')" ).closest('.post-wrapper.js_post-wrapper.wide.postlist-dense').remove();
-	$( ".text-upper:containsNC('" + person + "')" ).closest('.post-wrapper.js_post-wrapper.wide.postlist-dense').remove();
-	$( "a:containsNC('" + person + "')" ).closest('.post-wrapper.js_post-wrapper.postlist-dense').remove(); 
-	
-};
-
-
-chrome.runtime.onMessage.addListener(function(msg,sender){
-	if (msg.filter == "Send New Blocked List"){
-		
-		// Start Filtering Again After New Banned List Received
-			
-			PageLoadFiltering(msg.blocked);	
-
-	}
-			
->>>>>>> f0fc7c2cc635946f7e89bd11439dd7aba4dba886
 }); 
